d93dab3b2a627688d078a9b4b4cc8fc17bad3882
Brayam Ayala <b.ayalaga06@gmail.com>
2025-11-05 00:03:24 -0500

    Add `JpaUserRepository` and `UserPersistenceMapper` for user management persistence

---
+++ src/main/java/com/bag/complaint_system/identity/infrastructure/adapters/output/persistence/repository/JpaUserRepository.java
@@ -0,0 +1,18 @@
+package com.bag.complaint_system.identity.infrastructure.adapters.output.persistence.repository;
+
+import com.bag.complaint_system.identity.infrastructure.adapters.output.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
+
+  Optional<UserEntity> findByEmail(String email);
+
+  boolean existsByEmail(String email);
+
+  List<UserEntity> findAllByIsActive(Boolean isActive);
+
+  List<UserEntity> findAllByRole(UserEntity.RoleEntity role);
+}
