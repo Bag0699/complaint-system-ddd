a8fbb13d10a782023615b6d44e62d13baf8c6075
Brayam Ayala <b.ayalaga06@gmail.com>
2026-01-05 17:53:39 -0500

    Add Swagger annotations to request DTOs for improved API documentation

--- src/main/java/com/bag/complaint_system/complaint/application/dto/request/UpdateComplaintStatusRequest.java
+++ src/main/java/com/bag/complaint_system/complaint/application/dto/request/UpdateComplaintStatusRequest.java
@@ -1,5 +1,6 @@
 package com.bag.complaint_system.complaint.application.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,16 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UpdateComplaintStatusRequest {
 
+  @Schema(
+      description =
+          "The new status to be assigned to the complaint. Must be a valid state within the system workflow.",
+      example = "IN_PROGRESS",
+      allowableValues = {
+        "RECEIVED",
+        "IN_REVIEW",
+        "ACTION_TAKEN",
+        "CLOSED",
+      })
   @NotBlank(message = "The field New status cannot be blank or null.")
   private String newStatus;
 }
