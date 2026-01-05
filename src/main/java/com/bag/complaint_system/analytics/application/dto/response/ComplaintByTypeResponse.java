d6b069a8dc04aab5ec18b32da45255a7837ba150
Brayam Ayala <b.ayalaga06@gmail.com>
2026-01-05 17:53:13 -0500

    Add Swagger annotations to analytics response DTOs for better API documentation

--- src/main/java/com/bag/complaint_system/analytics/application/dto/response/ComplaintByTypeResponse.java
+++ src/main/java/com/bag/complaint_system/analytics/application/dto/response/ComplaintByTypeResponse.java
@@ -1,5 +1,6 @@
 package com.bag.complaint_system.analytics.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.Map;
@@ -10,6 +11,14 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ComplaintByTypeResponse {
+  @Schema(
+      description =
+          "A map containing complaint types as keys and their respective counts as values",
+      example = "{\"PHYSICAL\": 10, \"SEXUAL\": 5}")
   private Map<String, Long> data;
+
+  @Schema(
+      description = "The cumulative count of all complaints regardless of their type",
+      example = "15")
   private Long totalComplaints;
 }
